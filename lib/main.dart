ac7b234bc9966e94ae0f8d5a3d5407edc2360504
ataousCode <microservicestech97@gmail.com>
2025-04-26 17:44:21 +0800

    updated theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:vpnify/auth/register_screen.dart';
-import 'package:vpnify/screens/premium_screen.dart';
 import 'package:vpnify/theme/theme_provider.dart';
 
 import 'theme/app_theme.dart';
 
 void main() {
   runApp(
-      ChangeNotifierProvider(
-       create: (_) => ThemeProvider(),
-        child: const VPNMainApp(),
-      ),
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: const VPNMainApp(),
+    ),
   );
 }
 
@@ -24,7 +23,10 @@ class VPNMainApp extends StatelessWidget {
       builder: (context, themeProvider, _) {
         return MaterialApp(
           title: 'VPNIFY',
-          theme: themeProvider.isDarkMode ? AppTheme.darkTheme : AppTheme.lightTheme,
+          theme: AppTheme.darkTheme,
+          // themeProvider.isDarkMode
+          //     ? AppTheme.darkTheme
+          //     : AppTheme.lightTheme,
           home: const RegisterScreen(),
           debugShowCheckedModeBanner: false,
         );
